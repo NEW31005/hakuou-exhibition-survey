7bb6952a3d8ae02592905e35d5e473c3c3f1434f
NEW31005 <NEW31005@users.noreply.github.com>
2026-07-02 17:26:19 +0900

    Deploy static survey site

---
+++ _next/static/ZbvlsYo3ElBl4hGBm3Fle/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/hakuou-exhibition-survey//_next/:path+",
+        "destination": "/hakuou-exhibition-survey/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
